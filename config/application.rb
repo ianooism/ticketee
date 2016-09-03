6c2c498ae75d526e5d8875833b29f89933b92cca
Ian Ooi <ianooism@gmail.com>
2016-09-03 12:31:49 +0000

    customize generator

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,15 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Ticketee
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework  nil
+      g.assets  false
+      g.helper  false
+      g.jbuilder  false
+    end
   end
 end
