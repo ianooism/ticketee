2f1d84034eac99286eca39c73e89e7f39fa30794
Ian Ooi <ianooism@gmail.com>
2016-09-04 01:24:53 +0000

    add new action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,16 @@
 class ProjectsController < ApplicationController
+  # GET /projects
   def index
     @projects = Project.all
   end
   
+  # GET /projects/:id
   def show
     @project = Project.find(params[:id])
+  end
+  
+  # GET /projects/new
+  def new
+    @project = Project.new
   end
 end
