183a8ce2c46404dcc2c884bf8229deba32705349
Ian Ooi <ianooism@gmail.com>
2016-09-06 05:24:20 +0000

    add edit and update action

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,14 +1,13 @@
 class TicketsController < ApplicationController
   def show
     @project = set_project
-    @ticket = @project.tickets.find(params[:id])
+    @ticket = set_ticket
   end
   
   def new
     @project = set_project
     @ticket = @project.tickets.new
   end
-  
   def create
     @project = set_project
     @ticket = @project.tickets.create(ticket_params)
@@ -20,9 +19,27 @@ class TicketsController < ApplicationController
     end
   end
   
+  def edit
+    @project = set_project
+    @ticket = set_ticket
+  end
+  def update
+    @project = set_project
+    @ticket = set_ticket
+    if @ticket.update(ticket_params)
+      redirect_to project_path(@project),
+        notice: 'Ticket successfully updated.'
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def set_project
       Project.find(params[:project_id])
+    end
+    def set_ticket
+      @project.tickets.find(params[:id])
     end
     
     def ticket_params
