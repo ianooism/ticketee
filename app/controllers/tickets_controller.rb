3d0c2068179bea819a7edf38e8b15ed55fc94a42
Ian Ooi <ianooism@gmail.com>
2016-09-06 05:08:47 +0000

    add new and create action

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,31 @@
 class TicketsController < ApplicationController
   def show
-    @project = Project.find(params[:project_id])
+    @project = set_project
     @ticket = @project.tickets.find(params[:id])
   end
+  
+  def new
+    @project = set_project
+    @ticket = @project.tickets.new
+  end
+  
+  def create
+    @project = set_project
+    @ticket = @project.tickets.create(ticket_params)
+    if @ticket.save
+      redirect_to project_path(@project),
+        notice: 'Ticket successfully created.'
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def set_project
+      Project.find(params[:project_id])
+    end
+    
+    def ticket_params
+      params.require(:ticket).permit(:name, :description)
+    end
 end
