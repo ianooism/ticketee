78c16c11b24fca9f398f14adcd8ba606af394dcb
Ian Ooi <ianooism@gmail.com>
2016-09-12 13:25:35 +0000

    update devise

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_project, only: [:show, :edit, :destroy]
   
   def index
