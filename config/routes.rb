35a0e82615add076a19388bf75b71c0edb0448c5
Ian Ooi <ianooism@gmail.com>
2016-09-14 11:06:32 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,14 @@ Rails.application.routes.draw do
   
   devise_for :users, skip: [:registrations, :passwords]
   devise_scope :user do
-    resource :password, only: [:new, :create, :edit, :update],
-      as: 'user_password_reset',
-      path: 'users/password_reset',
-      controller: 'devise/passwords'
     namespace :users, as: 'user' do
       resource :registration, only: [:new, :create]
       resource :email, only: [:edit, :update]
       resource :password, only: [:edit, :update]
     end
+    resource :password_reset, only: [:new, :create, :edit, :update],
+      as: 'user_password_reset',
+      path: 'users/password_reset',
+      controller: 'devise/passwords'
   end
 end
