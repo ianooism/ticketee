5b1c9f7a1ab0fa8445ba6588b04e2b3fe2cb07df
Ian Ooi <ianooism@gmail.com>
2016-09-17 07:24:03 +0000

    move logic to model

--- app/controllers/users/emails_controller.rb
+++ app/controllers/users/emails_controller.rb
@@ -7,10 +7,6 @@ class Users::EmailsController < DeviseController
   
   def update
     if resource.update_with_password(user_params)
-      # TODO: logic belongs in model
-      #   if email entered = email in db, remove unconfirmed email
-      #   note that devise has code that keeps email the same even when changed
-      resource.update(unconfirmed_email: nil) if update_using_current_email?
       bypass_sign_in resource
       flash_key = resource.pending_reconfirmation? ?
         :update_email_needs_confirmation : :updated_email
@@ -30,10 +26,6 @@ class Users::EmailsController < DeviseController
   
     def user_params
       params.require(:user).permit(:email, :current_password)
-    end
-
-    def update_using_current_email?
-      params[:user][:email] == resource.email
     end
 
     def translation_scope
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable, :confirmable
   
+  before_update :remove_unconfirmed_email,
+    unless: Proc.new { |user| user.email_changed? }, prepend: true
+  
   validates :full_name, presence: true
+  
+  protected
+    
+    def remove_unconfirmed_email
+      self.unconfirmed_email = nil
+    end
 end
