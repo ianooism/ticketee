cb0ab96baf735ce49a421fba4cb8bd16e317ef39
Ian Ooi <ianooism@gmail.com>
2016-09-18 11:02:00 +0000

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
-  def get_error_message(object)
+  def get_errors_if_dev_env(object)
+    if Rails.env.development? && object.errors.any?
+      "Error: #{object.errors.messages}"
+    end
+  end
+  def get_error_message_for_form(object)
     if object.errors.any?
       I18n.t 'errors.messages.form_unsubmitted'
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160915044557) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
@@ -25,9 +28,7 @@ ActiveRecord::Schema.define(version: 20160915044557) do
     t.integer  "project_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "author_id"
-    t.index ["author_id"], name: "index_tickets_on_author_id"
-    t.index ["project_id"], name: "index_tickets_on_project_id"
+    t.index ["project_id"], name: "index_tickets_on_project_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,9 +49,10 @@ ActiveRecord::Schema.define(version: 20160915044557) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "full_name"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "tickets", "projects"
 end
