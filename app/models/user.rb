a65abd0fa7186eea1cc0669fec73462c0e42999b
Ian Ooi <ianooism@gmail.com>
2016-09-22 16:14:58 +0000

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   
   validates :full_name, presence: true
   
+  # override devise. if current email entered, cancel pending confirmation.
   before_update :remove_unconfirmed_email, prepend: true,
                 unless: proc { |user| user.email_changed? }
   
-  # extend devise to disallow blank passwords
+  # override devise. disallow blank passwords.
   def update_password(params)
     if not params[:password].blank? && params[:password_confirmation].blank?
       update_with_password(params)
@@ -17,7 +18,7 @@ class User < ApplicationRecord
     end
   end
   
-  protected
+  private
     
     def remove_unconfirmed_email
       self.unconfirmed_email = nil
