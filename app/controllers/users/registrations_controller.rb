6f532bf90b20b480ee6bbec923d821896b59fb8a
Ian Ooi <ianooism@gmail.com>
2016-09-23 04:01:41 +0000

    a

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,8 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  
   private
   
-    def configure_sign_up_params
+    def sign_up_params
       devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+      super
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   has_many :tickets, -> { order 'created_at desc' }, dependent: :delete_all
   
   alias_attribute :project_name, :name
-  
   validates :project_name, presence: true, uniqueness: true
+  
   validates :description, presence: true
 end
